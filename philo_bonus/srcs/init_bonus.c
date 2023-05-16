a6f95eed3762b9948dbff71c99e6cd4062649a25
Oumaima Halim <oumaimalmao23@gmail.com>
2023-05-16 03:25:22 +0100

    I fixed the hunging loop, I guess.

--- philo_bonus/srcs/init_bonus.c
+++ philo_bonus/srcs/init_bonus.c
@@ -6,7 +6,7 @@
 /*   By: ohalim <ohalim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/14 22:34:22 by ohalim            #+#    #+#             */
-/*   Updated: 2023/05/16 03:14:11 by ohalim           ###   ########.fr       */
+/*   Updated: 2023/05/16 03:17:22 by ohalim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,20 +20,16 @@ void	wait_for_processes(pid_t *pid, int nb_of_proc)
 	i = 0;
 	while (waitpid(-1, &exit_status, 0) != -1)
 	{
-		printf("in the while() wait in wait_for_processes\n");
 		if (WEXITSTATUS(exit_status) == 1)
 		{
 			while (i < nb_of_proc)
 			{
-				printf("in the while() kill in wait_for_processes\n");
 				kill(pid[i], SIGKILL);
 				i++;
 			}
-			printf("Killed processes and ready to exit the main onen\n");
 			exit (0);
 		}
 	}
-	printf("Out of while() in wait_for_processes\n");
 	exit (0);
 }
 
@@ -41,9 +37,7 @@ int	check_death(t_philo philo)
 {
 	if ((timestamp() - philo.last_meal) >= philo.data->t_to_die)
 	{
-		printf("%d waiting for display in check_death\n", philo.rank);
 		sem_wait(philo.data->display);
-		printf("%d waited for display in check_death\n", philo.rank);
 		printf("%lu ms %d %s\n", current_timestamp(philo.data->creation_time),
 			philo.rank, DIE);
 		return (FAILURE_RETURN);
--- philo_bonus/srcs/init_utils_bonus.c
+++ philo_bonus/srcs/init_utils_bonus.c
@@ -6,7 +6,7 @@
 /*   By: ohalim <ohalim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/14 22:34:20 by ohalim            #+#    #+#             */
-/*   Updated: 2023/05/16 02:39:35 by ohalim           ###   ########.fr       */
+/*   Updated: 2023/05/16 03:17:57 by ohalim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,53 +41,31 @@ long	current_timestamp(long start_time)
 
 void	ft_borintafo(t_philo *philo, char *def)
 {
-	printf("%d waiting for display in ft_borintafo\n", philo->rank);
 	sem_wait(philo->data->display);
-	printf("%d waited for display in ft_borintafo\n", philo->rank);
 	printf("%lu ms %d %s\n", current_timestamp(philo->data->creation_time),
 		philo->rank, def);
-	printf("%d posting for display in ft_borintafo\n", philo->rank);
 	sem_post(philo->data->display);
-	printf("%d posted for display in ft_borintafo\n", philo->rank);
 }
 
 void	__eat(t_philo *philo)
 {
-	printf("%d waiting for fork in __eat()\n", philo->rank);
 	sem_wait(philo->data->fork);
-	printf("%d waited for fork in __eat()\n", philo->rank);
 	ft_borintafo(philo, FORK);
-	printf("%d waiting for fork in __eat()\n", philo->rank);
 	sem_wait(philo->data->fork);
-	printf("%d waited for fork in __eat()\n", philo->rank);
 	ft_borintafo(philo, FORK);
 	ft_borintafo(philo, EAT);
 	if (philo->data->nb_of_circle > 0)
 		philo->circle--;
-	printf("%d's cirle is: %d\n", philo->rank, philo->circle);
 	if (philo->circle == 0)
 	{
-		printf("%d posting for fork in __eat()\n", philo->rank);
 		sem_post(philo->data->fork);
-		printf("%d posted for fork in __eat()\n", philo->rank);
-		printf("%d posting for fork in __eat()\n", philo->rank);
 		sem_post(philo->data->fork);
-		printf("%d posted for fork in __eat()\n", philo->rank);
-		printf("end of %d process\n", philo->rank);
 		exit (0);
 	}
-	printf("%d waiting for last_meal in __eat()\n", philo->rank);
 	sem_wait(philo->last_meal_s);
-	printf("%d waited for last_meal in __eat()\n", philo->rank);
 	philo->last_meal = timestamp();
-	printf("%d posting for last_meal in __eat()\n", philo->rank);
 	sem_post(philo->last_meal_s);
-	printf("%d posted for last_meal in __eat()\n", philo->rank);
 	ft_usleep(philo->data->t_to_eat);
-	printf("%d posting for fork in __eat()\n", philo->rank);
 	sem_post(philo->data->fork);
-	printf("%d posted for fork in __eat()\n", philo->rank);
-	printf("%d posting for fork in __eat()\n", philo->rank);
 	sem_post(philo->data->fork);
-	printf("%d posted for fork in __eat()\n", philo->rank);
 }
